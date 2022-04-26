1ccef2ef3b7f77749294c921c96b1fea2c5b1c50
nakayama <nakayama@jurabi.jp>
2022-04-26 09:49:00 +0900

    改行ができるように修正しました。

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
         <div class="item-detail">
           <div class="item-name">{{ name }}</div>
           <div class="item-message">
-            <p>{{ message }}</p>
+            <nl2br tag="div" :text="message" />
           </div>
         </div>
       </section>
@@ -43,10 +43,10 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/database'
 // 改行を <br> タグに変換するモジュール
-// import Nl2br from 'vue-nl2br'
+import Nl2br from 'vue3-nl2br'
 
 export default {
-  // components: { Nl2br },
+  components: { Nl2br },
   data() {
     return {
       user: {},  // ユーザー情報
@@ -170,6 +170,9 @@ export default {
   background: #deefe8;
   border-radius: 4px;
   line-height: 1.2em;
+}
+.item-message > div > br:first-of-type {
+  display: none;
 }
 .item-message::before {
   position: absolute;
